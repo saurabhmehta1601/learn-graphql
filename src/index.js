2a85d72b8576cebc005ae8bb7d99e3ba29b0786f
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-05-06 21:41:02 +0530

    query nested fields

--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
-const {ApolloServer,gql} = require("apollo-server")
+const { ApolloServer, gql } = require("apollo-server");
 
 // graphql schema
+// nesting schemas
+
 const typeDefs = gql`
-    type Query {
-        hello:String!
-    }
-`
+
+  type Query{
+    hello  : String!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+  }
+
+  type Error {
+    message: String
+  }
+
+  type RegisterResponse {
+    errors: [Error]
+    status: Int!
+  }
+
+  type Mutation {
+    register: RegisterResponse!
+  }
+`;
 
 // resolver
 const resolvers = {
-    Query:{
-        hello : () => "hello World !!"
-    }
-}
+  Query: {
+    hello: () => "hello graphql !!",
+  },
+  Mutation: {
+    register: () => ({
+      errors: null,
+      status: 200 ,
+    }),
+  },
+};
 
-// server instance 
-const server = new ApolloServer({typeDefs,resolvers})
+// server instance
+const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({url})=> console.log(`>Apollo server runnning at ${url}`))
+server
+  .listen()
+  .then(({ url }) => console.log(`>Apollo server runnning at ${url}`));
