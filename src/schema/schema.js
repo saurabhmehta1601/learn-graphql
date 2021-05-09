220043601994899bdb7bc97b111d13afdd3b12cb
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-05-09 20:58:39 +0530

    add author book schema

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,17 +1,27 @@
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = require("graphql");
 
-const dummyBook = {
-  id:"1",
-  name: "Your dummy book",
-  genre : "No genra "
-}
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -21,17 +31,28 @@ const RootQuery = new GraphQLObjectType({
     book: {
       type: BookType,
       args: {
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
       },
-      resolve(parent, args) {
+      resolve(parent, {id,name,genre}) {
         // resolver function for query Book"
-        return dummyBook
+        return { id,name,genre };
       },
     },
+    author : {
+      type : AuthorType,
+      args : { 
+        id: {type : GraphQLID},
+        name: {type : GraphQLString},
+        age: {type : GraphQLInt},
+    },
+      resolve(parent,{id,name,age}){
+        return { id,name,age }
+      }
+    }
   },
 });
-
-
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
