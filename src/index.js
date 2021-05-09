af5374d726b2efc96161ff97c6beef88d196cdb2
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-05-09 20:13:43 +0530

    --amend

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express  =require("express")
-const { graphql } = require("graphql")
+const { graphqlHTTP } = require("express-graphql")
 const app = express()
 const schema  = require("./schema/schema")
 
-
+app.use("/graphql",graphqlHTTP({
+    schema
+}))
 
 app.listen(4000,()=> console.log('> Express server running on port 4000'))
